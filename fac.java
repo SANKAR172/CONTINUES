b75de86971a81c911d64fc3d826288f087e3bae7
SANKAR172 <124964711+SANKAR172@users.noreply.github.com>
2024-01-04 12:09:37 +0530

    Update fac.java

--- fac.java
+++ fac.java
@@ -1,17 +1,17 @@
-public class Factorial {
+public class fac{
 
    public static void main(String[] args) {
        int num1 = 5;
        int num2 = 10;
 
-       long result1 = factorial(num1);
-       long result2 = factorial(num2);
+       long result1 = fac(num1);
+       long result2 = fac(num2);
 
-       System.out.println("Factorial of " + num1 + " is " + result1);
-       System.out.println("Factorial of " + num2 + " is " + result2);
+       System.out.println("fac of " + num1 + " is " + result1);
+       System.out.println("fac of " + num2 + " is " + result2);
    }
 
-   public static long factorial(int num) {
+   public static long fac(int num) {
        long result = 1;
        for (int i = 1; i <= num; i++) {
            result = result * i;
